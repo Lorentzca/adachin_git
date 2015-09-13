ebc60b274eca8ccddd2f2506253d11b0c7a216ca
adachiryou <adachiryou@RVIRUS-Macmini>
2015-09-13 16:27:44 +0900

    first commit

--- iOS/MyPlayground.playground/Contents.swift
+++ iOS/MyPlayground.playground/Contents.swift
@@ -10,4 +10,16 @@ let tax = 1.08
 //繰り返し
 for n in 1...9 {
     println(3 * n)
+    println(4 * n)
+}
+
+var sum = 0
+for n in 1...100 {
+    sum = sum + n
+}
+println(sum)
+for x in 1...9 {
+    for n in 1...9 {
+        println(x * n)
+    }
 }
