e5c2519de2cf0b0c93237470113b5f59f10fe320
adachi <adachi@ad-cloud.jp>
2015-09-28 19:31:15 +0900

    first commit

---
+++ itamae/recipe/vagrant/user_recipe.rb
@@ -0,0 +1,25 @@
+user "adachin" do
+   action :create
+ #  uid "502"
+ #  gid "508"
+   username "adachin"
+   home "/home/adachin"
+   password "hoge"
+ end
+ 
+ #make a key
+ USER_NAME = "adachin"
+ SSH_KEY = "ssh-rsa ~"
+
+ directory "/home/#{USER_NAME}/.ssh" do
+ end
+
+ file "/home/#{USER_NAME}/.ssh/authorized_keys" do
+ end
+
+ file "/home/#{USER_NAME}/.ssh/authorized_keys" do
+   content SSH_KEY
+   owner USER_NAME
+   group USER_NAME
+   mode  "600"
+ end
