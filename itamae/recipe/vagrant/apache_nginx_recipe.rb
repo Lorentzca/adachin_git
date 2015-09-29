2caa9a4d7cf49d47226a83d308f16372b0fd16f1
adachi <adachi@ad-cloud.jp>
2015-09-29 12:41:12 +0900

    first commit

--- itamae/recipe/vagrant/apache_nginx_recipe.rb
+++ itamae/recipe/vagrant/apache_nginx_recipe.rb
@@ -1,7 +1,12 @@
+#nginx
 package 'http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm' do
   not_if 'rpm -q nginx-release-centos-6-0.el6.ngx.noarch'
 end
 
 package "nginx" do
   action :install
+end
+
+#apache
+package "httpd" do
 end
