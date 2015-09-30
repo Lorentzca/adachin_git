d6d6b9a8585ccd22c7edf19c7307fcb9f450b685
adachi <adachi@ad-cloud.jp>
2015-09-30 18:06:35 +0900

    first commit

---
+++ itamae/recipe/vagrant/first_package_recipe.rb
@@ -0,0 +1,24 @@
+package "git" do
+  action :install
+end
+
+package "gcc" do
+  action :install
+end
+
+package "openssl-devel" do
+  action :install
+end
+
+package "readline-devel" do
+  action :install
+end
+
+package "zlib-devel" do
+  action :install
+end
+
+package "gcc-c++" do
+  action :install
+end
+
--- itamae/recipe/vagrant/package_recipe.rb
+++
@@ -1,4 +0,0 @@
-execute "git install" do 
-   user "root" 
-    command "yum -y install git gcc openssl-devel readline-devel zlib-devel gcc-c++" 
-end
