aa323806fd125fc37d71323c4653c8bc4083ed13
RVIRUS <adachiryou@RVIRUS-MacBookPro.local>
2015-10-12 18:58:33 +0900

    first commit

--- itamae/recipe/vagrant/htop_recipe.rb
+++
@@ -1,7 +0,0 @@
-package 'http://ftp-srv2.kddilabs.jp/Linux/distributions/fedora/epel/6/x86_64/epel-release-6-8.noarch.rpm' do
-  not_if 'rpm -q epel-release-6-8.noarch.rpm '
-end
-
-package "htop" do
-  action :install
-end
--- itamae/recipe/vagrant/tmux_recipe.rb
+++
@@ -1,7 +0,0 @@
-package 'http://pkgs.repoforge.org/rpmforge-release/rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm' do
-  not_if 'rpm -ivh rpmforge-release-0.5.3-1.el6.rf.x86_64.rpm'
-end
-
-package "tmux" do
-  action :install
-end
