36b85c0004b2ce9c032660e2f1fb5cb78eed0a39
adachi <adachi@ad-cloud.jp>
2016-10-11 15:36:57 +0900

    ansible docker modify

--- Docker/pythoninstall.sh
+++ Docker/pythoninstall.sh
@@ -2,17 +2,16 @@
 VERSIONS=${VERSIONS:-"2.7.8.10"}
 
 # make directory
-mkdir -p /opt/bin
-cd /opt
+mkdir -p /bin
+cd /bin
 
 wget http://downloads.activestate.com/ActivePython/releases/${VERSIONS}/ActivePython-${VERSIONS}-linux-x86_64.tar.gz
 tar -xzvf ActivePython-${VERSIONS}-linux-x86_64.tar.gz
 
-mv ActivePython-${VERSIONS}-linux-x86_64 apy && cd apy && ./install.sh -I /opt/python/
+mv ActivePython-${VERSIONS}-linux-x86_64 apy && cd apy && ./install.sh -I /bin/python/
 
-ln -s /opt/python/bin/easy_install /opt/bin/easy_install
-ln -s /opt/python/bin/pip /opt/bin/pip
-ln -s /opt/python/bin/python /opt/bin/python
-ln -s /opt/python/bin/virtualenv /opt/bin/virtualenv
-ln -s /opt/python/bin/python /bin/python
+ln -s /opt/python/bin/easy_install /bin/easy_install
 ln -s /opt/python/bin/pip /bin/pip
+ln -s /opt/python/bin/python /bin/python
+ln -s /opt/python/bin/virtualenv /bin/virtualenv
+ln -s /opt/python/bin/python /usr/bin/python
