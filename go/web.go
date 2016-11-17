99828fb8b149145a74c6ce24c376671ae745941f
adachi <adachi@ad-cloud.jp>
2016-11-17 12:27:07 +0900

    go add web

--- go/web.go
+++ go/web.go
@@ -4,7 +4,7 @@ import (
     "net/http"
 )
 func main() {
-    http.Handle("/", http.FileServer(http.Dir("static")))
+    http.Handle("/", http.FileServer(http.Dir("index")))
     err := http.ListenAndServe(":8080", nil)
     if err != nil {
         log.Fatal("ListenAndServe: ", err)
