18db33a4d35edcbd29184efe9f77210cdc3be4d4
RVIRUS0817 <t.virus.1989@gmail.com>
2016-12-18 15:42:48 +0900

    remove lambda

--- lambda/terminate-instance.py
+++
@@ -1,9 +0,0 @@
-#特定インスタンスを削除する場合
-import boto3
- 
-def lambda_handler(event, context):
-    client = boto3.client('ec2')
-    response = client.terminate_instances(
-        InstanceIds=['xxxxxxxxx']
-        )
-    print  response
